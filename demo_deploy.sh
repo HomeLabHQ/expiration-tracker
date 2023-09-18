c9ec13af60f6d0e8e6b0031908ec3dfa21021bbf
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-18 17:56:19 +0300

    Add recreation of container

--- demo_deploy.sh
+++ demo_deploy.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 
 git pull
-docker compose -f compose.override.yml up -d --build
+docker compose -f compose.override.yml up -d --build --force-recreate
 docker exec -it expiration-tracker-api python manage.py migrate
 docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input
 setsid devtunnel host ${DEMO_TUNNEL} >/dev/null 2>&1 < /dev/null &
