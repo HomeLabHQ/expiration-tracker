5a4e2368850814f247bb9d053905eecc3004df0e
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-18 17:54:32 +0300

    Chain commands

--- demo_deploy.sh
+++ demo_deploy.sh
@@ -3,6 +3,7 @@
 
 git pull
 docker compose -f compose.override.yml up -d --build
-docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input
+docker exec -it expiration-tracker-api python manage.py migrate && \
+docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input &&\
 setsid devtunnel host ${DEMO_TUNNEL} >/dev/null 2>&1 < /dev/null &
 exec "$@"
