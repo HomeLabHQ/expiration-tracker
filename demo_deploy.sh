5428e5d86edb495c098303add4b8c28999771208
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-18 17:41:24 +0300

    Move deploy to script

---
+++ demo_deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#!/bin/sh
+
+git pull
+docker compose -f compose.override.yml up -d --build
+docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input
+setsid devtunnel host ${DEMO_TUNNEL} >/dev/null 2>&1 < /dev/null &
+exec "$@"
