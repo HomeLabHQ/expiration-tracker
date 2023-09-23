40974a57f50f3eab17d46ff3db6755a47664cedc
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-23 10:08:09 +0300

    Fix issue with demo deploy

--- demo_deploy.sh
+++ demo_deploy.sh
@@ -3,5 +3,7 @@
 
 git pull
 docker compose -f compose.override.yml up -d --build --force-recreate
+docker exec -it expiration-tracker-api python manage.py migrate
+docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input
 setsid devtunnel host ${TUNNEL_ID} >/dev/null 2>&1 < /dev/null &
 exec "$@"
