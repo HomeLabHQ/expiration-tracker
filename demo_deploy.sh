63ec4f9cb329be4b53c8363dc8d9f071901c382e
oleksandr.korol <oleksandr.korol@coaxsoft.com>
2023-09-24 11:00:12 +0300

    Update demo_deploy.sh

--- demo_deploy.sh
+++ demo_deploy.sh
@@ -2,7 +2,8 @@
 #!/bin/sh
 
 git pull
-docker compose -f compose.override.yml up -d --build
+docker compose -f compose.override.yml build
+docker compose -f compose.override.yml up -d
 docker exec -it expiration-tracker-api python manage.py migrate
 docker exec -it expiration-tracker-api python manage.py createsuperuser --no-input
 setsid devtunnel host ${TUNNEL_ID} >/dev/null 2>&1 < /dev/null &
